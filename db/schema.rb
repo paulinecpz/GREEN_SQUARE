1c66c96773d73d4f2cbe64b149317047f49b783f
Pauline Coupez <coupez.pauline@gmail.com>
2019-11-18 16:24:28 +0000

    schema done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_153029) do
+ActiveRecord::Schema.define(version: 2019_11_18_162321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.bigint "garden_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["garden_id"], name: "index_bookings_on_garden_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "gardens", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "description"
+    t.integer "price_per_day"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_gardens_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +58,8 @@ ActiveRecord::Schema.define(version: 2019_11_18_153029) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "gardens"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "gardens", "users"
+  add_foreign_key "reviews", "bookings"
 end
