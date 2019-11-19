028e850e77e91c7102bfeebc85032cbae7071c73
Eduardo Borges <edumlborges@gmail.com>
2019-11-19 11:46:03 +0000

    Authorizations for gardens and add admin column to users

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,22 +1,46 @@
 class GardensController < ApplicationController
+  before_action :set_garden, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show, :index]
   def index
+    @garden = Garden.all
+    @garden = policy_scope(Garden).order(:name)
   end
-
   def show
   end
-
   def new
+    @garden = Garden.new
+    authorize @garden
   end
-
   def create
-  end
+    @garden = Garden.new(garden_params)
+    authorize @garden
 
+    if @garden.save
+      redirect_to garden_path(@garden), notice: 'Garden was successfully created'
+    else
+      render :new
+    end
+  end
   def edit
   end
-
   def update
+    if @garden.update(garden_params)
+    redirect_to garden_path(@garden), notice: 'Garden was successfully updated'
   end
-
   def destroy
+    if @garden.user == current_user || current_user.admin?
+      @garden.destroy
+    redirect_to gardens_path, notice: 'Garden was successfully destroyed'
+  else
+  end
+  end
+private
+  def set_garden
+    @garden = Garden.find(params[:id])
+    authorize @garden
+  end
+  def garden_params
+    params.require(:garden).permit(:name, :address, :description, :price_per_day, :user_id)
   end
 end
+
