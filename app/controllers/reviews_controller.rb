48a9117bd311c0549a0bb59942428adf0b920614
Eduardo Borges <edumlborges@gmail.com>
2019-11-19 14:03:26 +0000

    Review Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+  def new
+    @garden = Garden.find(params[:garden_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    @garden = Garden.find(params[:garden_id])
+    @review.garden = @garden
+    @review.save
+    redirect_to garden_path(@garden)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
