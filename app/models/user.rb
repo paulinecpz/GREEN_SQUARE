4e4652ac36e11c5685d89d3cf44918d64a03ca20
Pauline Coupez <coupez.pauline@gmail.com>
2019-11-21 11:39:16 +0000

    signup fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,5 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :reviews, through: :bookings
 
-  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 end
