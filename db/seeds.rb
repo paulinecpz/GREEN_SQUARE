d7ee2b0e6295475bd93cdb9a6a440ca6f2cebde1
Paulo Marques <bareu5@gmail.com>
2019-11-21 17:37:11 +0000

    user profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,14 +28,14 @@ USERS = []
       email: Faker::Internet.email,
       name: Faker::Name.name,
       password: "password",
-      avatar: Faker::Avatar.image("my-own-slug"),
+      remote_avatar_url: Faker::Avatar.image,
       admin: false
       )
   end
   ids = []
   User.all.each { |user| ids << user.id  }
 
-  images = ["1.jpg","2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "12.jpg", "13.jpg", "15.jpg", "16.jpg", "18.jpg", "19.jpg","20.jpg", "21.jpg", "22.jpg" ] 
+  images = ["1.jpg","2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "12.jpg", "13.jpg", "15.jpg", "16.jpg", "18.jpg", "19.jpg","20.jpg", "21.jpg", "22.jpg" ]
 
   20.times do
     Garden.create!(name: Faker::Cannabis.buzzword,
