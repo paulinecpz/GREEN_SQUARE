a42085e09418383f5487ad8ef508e6228e8cc312
Mathieu Fontaine <mathieu.antoine.fontaine@gmail.com>
2019-11-21 16:40:06 +0000

    map and sticky nav bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,11 @@
 import "bootstrap";
+
+// CSS
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
+import { initMapbox } from '../plugins/init_mapbox.js';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+// import { initAutocomplete } from '../plugins/init_autocomplete.js';
+
+initMapbox();
+// initAutocomplete();
