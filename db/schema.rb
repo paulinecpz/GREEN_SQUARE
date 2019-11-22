8718a1058c04087fe06ac4977762b66f6275e012
Pauline Coupez <coupez.pauline@gmail.com>
2019-11-22 12:37:16 +0000

    review working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_155045) do
+ActiveRecord::Schema.define(version: 2019_11_22_112443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,9 @@ ActiveRecord::Schema.define(version: 2019_11_21_155045) do
     t.bigint "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "garden_id"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["garden_id"], name: "index_reviews_on_garden_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,4 +72,5 @@ ActiveRecord::Schema.define(version: 2019_11_21_155045) do
   add_foreign_key "bookings", "users"
   add_foreign_key "gardens", "users"
   add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "gardens"
 end
