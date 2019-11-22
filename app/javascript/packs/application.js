d8a195d1caaeeacf459251a0e29ecdecbdf48ea5
Eduardo Borges <edumlborges@gmail.com>
2019-11-22 11:15:21 +0000

    datepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,16 +5,16 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox.js';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { initDatepicker } from '../plugins/datepickers.js';
 import { initAutocomplete } from '../plugins/init_autocomplete.js';
 
 initMapbox();
 initAutocomplete();
 
-import { initDatepicker } from '../plugins/datepicker';
-import "../plugins/flatpickr"
+// import "../plugins/flatpickr"
 
 initDatepicker();
 
 
-import "../plugins/flatpickr"
+
 
