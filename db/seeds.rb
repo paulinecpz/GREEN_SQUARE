c648c09888d8e7bec790af57da580319d2fe9f4b
Pauline Coupez <coupez.pauline@gmail.com>
2019-11-22 16:47:17 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,47 +10,26 @@ require 'faker'
 puts("deleting gardens")
 
 
+Booking.destroy_all
 Review.destroy_all
 Garden.destroy_all
-Booking.destroy_all
 User.destroy_all
 
-User.create(name: "Paulo", email:"paulo@paulo.com", password:"password", admin: true)
-User.create(name: "Pauline", email:"pauline@pauline.com", password:"password", admin: true)
-User.create(name: "Mathieu", email:"mathieu@mathieu.com", password:"password", admin: true)
-User.create(name: "Eduardo", email:"edu@edu.com", password:"password", admin: true)
-
+puts("creating gardens and users")
+User.create(name: "Paulo Marques", email:"paulo_marques@gmail.com", password:"password", admin: true)
+User.create(name: "Pauline Coupez", email:"pauline_coupez@gmail.com", password:"password", admin: true)
+User.create(name: "Mathieu Fontaine", email:"mathieu_fontaine@gmail.com", password:"password", admin: true)
+User.create(name: "Eduardo Borges", email:"eduardo_borges@gmail.com", password:"password", admin: true)
 
 # users= [user1, user2, user3, user4]
-puts("creating gardens and users")
-
-USERS = []
-15.times do
-    USERS << User.create!(
-      email: Faker::Internet.email,
-      name: Faker::Name.name,
-      password: "password",
-      remote_avatar_url: Faker::Avatar.image,
-      admin: false
-      )
-  end
-  ids = []
-  User.all.each { |user| ids << user.id  }
-
-  images = ["1.jpg","2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "12.jpg", "13.jpg", "15.jpg", "16.jpg", "18.jpg", "19.jpg","20.jpg", "21.jpg", "22.jpg" ]
-
-  20.times do
-    Garden.create!(name: Faker::Cannabis.buzzword,
-      address: Faker::Address.city,
-      description: Faker::Lorem.paragraph,
-      price_per_day: Faker::Number.within(range: 5..20),
-      photo: images.sample,
-      user_id: ids.sample
-      )
-  end
-
-
-
+images = ["1.jpg","2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "12.jpg", "13.jpg", "15.jpg", "16.jpg", "18.jpg", "19.jpg","20.jpg", "21.jpg", "22.jpg" ]
+Garden.create(name: "Jardim Botânico", address: "Calçada Martim de Freitas, Coimbra", description: "A beautiful garden located in the city center of one of the most famous citys in Portugal.", price_per_day: 100, photo: images.sample, user_id: 2)
+Garden.create(name: "Jardim d'Ajuda", address: "Calçada da Ajuda, Lisboa", description: "A huge garden with lots of space ideal to grow cool things and for long rents terms", price_per_day: 125, photo: images.sample, user_id: 2)
+Garden.create(name: "Estufa Fria", address: "Parque Eduardo VII, Lisboa", description: "Garden with one of the best views in town. Perfect to do whatever you want to.", price_per_day: 155, photo: images.sample, user_id: 2)
+Garden.create(name: "Jardim Manuel Bivar", address: "Praça Dom Francisco Gomes, Faro", description: "Elegant brand new garden in Faro. Located near the best beaches in south of Portugal.", price_per_day: 135, photo: images.sample, user_id: 2)
+Garden.create(name: "Jardim do Palácio dos Távoras", address: "Largo Igreja, Mirandela", description: "Located outside Mirandela it's the perfect place to throw a party.", price_per_day: 100, photo: images.sample, user_id: 2)
+Garden.create(name: "Jardim da Avenida Central", address: "Avenida Central, Braga", description: "Located in a historic city in the north of Portugal. Perfect weather conditions to grow", price_per_day: 120, photo: images.sample, user_id: 2)
+Garden.create(name: "Parque do Castelo e Paço dos Duques de Bragança", address: "Rua Conde Dom Henrique, Guimarães", description: "Garnde in the north of Portugal. Perfect weather to grow stuff.", price_per_day: 200, photo: images.sample, user_id: 2)
 
 
 puts("seed finished")
